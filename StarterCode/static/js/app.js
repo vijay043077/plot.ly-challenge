a9ffefd2fb7ff9bfe0a3020eb67de116bc82c32e
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-07-28 23:03:12 -0500

    commit3

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,13 @@
-function samplePlot() {
+function samplePlot(id) {
 
-    d3.json("samples.json").then(function(response) {
-        
+    d3.json("samples.json").then(function (response) {
+
         console.log(response);
 
         const samples = response.samples;
 
-        const ids = samples.filter(data => data.id == samplePlot);
-
-        const display = ids[0]
+        const display = samples.filter(samples => samples.id == id)[0];
+        console.log(display)
 
         const sample_values = display.sample_values;
         console.log(sample_values);
@@ -22,16 +21,20 @@ function samplePlot() {
         const topTen_values = sample_values.slice(0, 10);
         console.log(topTen_values);
 
-        const topTen_ids = otu_ids.slice(1, 10);
+        const topTen_ids = otu_ids.slice(0, 10);
         console.log(topTen_ids);
 
-        const topTen_labels = otu_labels.slice(1, 10);
+        const topTen_labels = otu_labels.slice(0, 10);
         console.log(topTen_labels);
 
+        const displayLabels = otu_ids.map(id => "OTU" + " " + id)
+        console.log(`OTU ID: ${otu_ids}`);
+        console.log(`OTU_labels: ${otu_labels}`);
+
         const trace1 = {
-            x: topTen_values,
-            y: topTen_ids,
-            text: topTen_labels,
+            x: topTen_values.reverse(),
+            y: displayLabels.reverse(),
+            text: topTen_labels.reverse(),
             type: "bar",
             orientation: "h"
         };
@@ -53,4 +56,17 @@ function samplePlot() {
     });
 }
 
-//samplePlot();
+let dropDown = d3.select("#selDataset")
+d3.json("samples.json").then(function (response) {
+    let names = response.names;
+
+    names.forEach((name) => {
+        dropDown.append("option").text(name).property("value", name);
+
+    });
+});
+samplePlot(940);
+
+function optionChanged(value) {
+    samplePlot(value)
+}
